d3e68b4e6e9520b3ded6cccea73f0ad7534e5dd1
fr0ment <panshinilya2@gmail.com>
2025-06-17 21:27:53 +0500

    update

--- migrations/versions/aa03c32881d9_add_average_rating_and_order_items.py
+++ migrations/versions/aa03c32881d9_add_average_rating_and_order_items.py
@@ -19,8 +19,16 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
-
+    op.add_column('products', sa.Column('average_rating', sa.Float(), nullable=True))
+    op.create_table(
+        'order_items',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('order_id', sa.Integer(), sa.ForeignKey('orders.id'), nullable=False),
+        sa.Column('product_id', sa.Integer(), sa.ForeignKey('products.id'), nullable=False),
+        sa.Column('quantity', sa.Integer(), nullable=False, default=1),
+        sa.PrimaryKeyConstraint('id')
+    )
 
 def downgrade() -> None:
-    pass
+    op.drop_column('products', 'average_rating')
+    op.drop_table('order_items')
